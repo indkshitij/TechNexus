5d14a91821ab4a4af407f0147296206b63958793
Kshitij Singh <138932243+indkshitij@users.noreply.github.com>
2023-07-09 02:45:33 +0530

    Added Firebase

--- login.js
+++ login.js
@@ -1,58 +1,73 @@
-// Firebase
-console.log("hiii")
+// // Firebase
+// console.log("hiii")
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 
-// import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
-// https://firebase.google.com/docs/web/setup#available-libraries
+// //  import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
+// //  const database = firebase.database()
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBMLIRRVrwKDBUmJDFX_keiWHlHY_YTOhM",
-  authDomain: "technexus-8922a.firebaseapp.com",
-  projectId: "technexus-8922a",
-  storageBucket: "technexus-8922a.appspot.com",
-  messagingSenderId: "684302873445",
-  appId: "1:684302873445:web:29593df706b63d76f6a78c"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBMLIRRVrwKDBUmJDFX_keiWHlHY_YTOhM",
+//   authDomain: "technexus-8922a.firebaseapp.com",
+//   projectId: "technexus-8922a",
+//   storageBucket: "technexus-8922a.appspot.com",
+//   messagingSenderId: "684302873445",
+//   appId: "1:684302873445:web:29593df706b63d76f6a78c"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
-// const auth = getAuth();
+//  const auth = getAuth();
 
 
+// window.login = function(e) {
+//   e.preventDefault(); 
+
+//   function signInWithEmailPassword() {
+//     var email = "test@example.com";
+//     var password = "hunter2";
+//     // [START auth_signin_password]
+//     auth.signInWithEmailAndPassword(email, password)
+//       .then((userCredential) => {
+//         // Signed in
+//         var user = userCredential.user;
+//         alert("hb")
+//         // ...
+//       })
+//       .catch((error) => {
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//       });
+// };
+  
+// }
 // Firebase configuration and initialization (already included in your code)
 
-// Assuming you have included the Firebase SDK and Swal library in your HTML file
+// Assuming you have included the Firebase SDK in your HTML file
 
-const loginButton = document.getElementById("login");
-loginButton.addEventListener("click", function(e) {
+window.login = function(e) {
   e.preventDefault();
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Login successful
-      Swal.fire({
-        title: "Welcome back!",
-        text: "You have successfully logged in!",
-        icon: "success"
-      }).then(() => {
-        // Redirect to a different page
-        document.location.href = "inde.html";
-      });
+      var user = userCredential.user;
+      alert("Login successful!");
+      // Redirect to a different page
+      document.location.href = "./index.html";
     })
     .catch((error) => {
       // Login failed
-      Swal.fire({
-        title: "Login failed",
-        text: "Invalid email or password",
-        icon: "error"
-      });
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert("Error: " + errorMessage);
     });
-});
+};
